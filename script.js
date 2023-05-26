beb5c1c3f69fa1dea02081aae9671ab842a55460
codingwithxen <ranjuhansda333@gmail.com>
2023-05-26 20:56:59 +0530

    added login-register

---
+++ script.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+registerLink.addEventListener('click', ()=> {wrapper.classList.add('active');});
+
+loginLink.addEventListener('click',()=> {wrapper.classList.remove('active');});
+
+btnPopup.addEventListener('click',()=> {wrapper.classList.add('active-popup');
+document.body.classList.add('blur');
+
+            });      
+iconClose.addEventListener('click',()=> {wrapper.classList.remove('active-popup');
+document.body.classList.remove('blur');
+
+});      
+
+if ( wrapper.classList.contains('active-popup') ) {
+ 
+} 
+console.log(wrapper.classList)
+console.log(document.body.classList)
