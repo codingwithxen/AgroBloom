76fbe34ac90103a717a4f1e90eb74184546f276e
codingwithxen <ranjuhansda333@gmail.com>
2023-05-30 12:40:13 +0530

    added footer

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const blur = document.querySelector('.blur')
+
 
 
 registerLink.addEventListener('click', () => { wrapper.classList.add('active'); });
